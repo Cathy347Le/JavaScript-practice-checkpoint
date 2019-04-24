38b3b4d57fb534f641f602838bbe696234d48938
Cathy <Cathy347Le@gmail.com>
2019-04-24 01:23:09 -0400

    added hof.js exercise

---
+++ hof.js
@@ -0,0 +1,41 @@
+// NOTE: Make sure to use the `var` keyword for ALL variable declarations
+
+var people = [
+  {
+    name: "Layla",
+    age: 27,
+    course: "WDI"
+  },
+  {
+    name: "Keanu",
+    age: 54,
+    course: "WDI"
+  },
+  {
+    name: "Jasmine",
+    age: 35,
+    course: "DSI"
+  }
+];
+
+// #1: Use the `map` array method to create a new array containing the ages of each
+// person in the `people` array. Assign the returned array to a variable
+// called `peopleAges`.
+// Type your solution immediately below this line:
+
+var peopleAges = people.map(function(person) {
+  return person.age;
+});
+
+// #2: Use the `filter` array method to create a new, filtered array containing only
+// persons from the `people` array who are WDI students. Assign the returned array
+// to a variable called `wdiStudents`.
+// Type your solution immediately below this line:
+
+var wdiStudents = people.filter(function(person) {
+  if (person.course === "WDI") {
+    return true;
+  }
+});
+
+// console.log(wdiStudents);
